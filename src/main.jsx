204a0bafa2667d6a2376203cb2cf5869ea2fe70f
João <joaopedroolive@hotmail.com>
2024-01-05 08:34:03 -0300

    SignUp & Profile page

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from './styles/global';
 
 import theme from './styles/theme';
 
-import { SignIn } from './pages/SignIn';
+import { Profile } from './pages/Profile';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-        <SignIn />
+        <Profile />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,39 @@
+import { Container, Form, Avatar } from "./styles";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi"
+
+export function Profile(){
+    return(
+        <Container>
+            <header>
+                <a href="#"><FiArrowLeft /></a>
+            </header>
+
+
+            <Form>
+            <Avatar>
+                <img src="https://github.com/JoaoPedroOM.png" alt="Foto de perfil do usuário" />
+
+                <label htmlFor="avatar">
+                    <FiCamera />
+
+                    <input id="avatar" type="file" />
+                </label>
+            </Avatar>
+
+            <Input placeholder="Nome" type="text" icon={FiUser}/>
+
+            <Input placeholder="E-mail" type="text" icon={FiMail}/>
+
+            <Input placeholder="Senha atual" type="password" icon={FiLock}/>
+
+            <Input placeholder="Nova senha" type="password" icon={FiLock}/>
+
+            <Button title="Salvar"/>
+
+            </Form>
+        </Container>
+    )
+}
